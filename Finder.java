2a0a34b59463788e714d0bdf26e4b5890a996103
Avakian2004Lilit <114296828+Avakian2004Lilit@users.noreply.github.com>
2022-10-03 20:37:57 +0200

    Add files via upload

---
+++ Finder.java
@@ -0,0 +1,24 @@
+package classwork4;
+
+import java.util.Scanner;
+
+
+public class Finder {
+    static String storage = "I Love Java";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String searchPhrase = scanner.next();
+        System.out.println(storage.indexOf(searchPhrase));
+
+
+
+        }
+
+
+
+
+
+        }
+
+
