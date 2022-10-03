c2f82ba2b5233b4afb8494a95da81de0eda345af
Avakian2004Lilit <114296828+Avakian2004Lilit@users.noreply.github.com>
2022-10-03 20:34:29 +0200

    Add files via upload

---
+++ CW3.java
@@ -0,0 +1,26 @@
+package classwork3;
+
+import java.beans.JavaBean;
+
+public class CW3 {
+    public static void main(String[] args) {
+    String studyBasicJava = "study Basic Java!";
+    System.out.println(studyBasicJava);
+    System.out.println(studyBasicJava.charAt(16));
+    System.out.println(studyBasicJava.contains("Java"));
+    System.out.println(studyBasicJava.replace('a','o'));
+    System.out.println(studyBasicJava.substring(12));
+
+
+
+    double percent = (200 / 470.0) * 100;
+    System.out.println(percent);
+
+    char a = 'g';
+    int i = a;
+    System.out.println(i);
+
+
+    }
+
+}
