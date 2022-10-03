42e5c0c8c2deeba22a4b36939e99f837db1eaf83
Avakian2004Lilit <114296828+Avakian2004Lilit@users.noreply.github.com>
2022-10-03 20:31:36 +0200

    Add files via upload

---
+++ CW2.java
@@ -0,0 +1,25 @@
+package classwork2;
+
+import java.util.Scanner;
+
+public class CW2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Lilit !");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello Lilit");
+        String phrasel = sc.next();
+        System.out.println(phrasel);
+
+        double number = 59.976;
+        int numberInt = (int) Math.floor(number) ;
+        System.out.println("numberInt");
+
+        int i =475;
+        short s =(short) i;
+        System.out.println(i);
+
+
+
+
+    }
+}
